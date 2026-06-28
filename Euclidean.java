fa9391d3385bb00cb062b6040ba63143a4deb31e
Phoenix <140335324+fashaap@users.noreply.github.com>
2026-06-28 23:30:32 +0700

    Dubigeus genug

--- Euclidean.java
+++ Euclidean.java
@@ -1,17 +1,35 @@
 public class Main {
+
     public static void main(String[] args) {
-        int x = 19;
-        int y = 2;
 
-        while (y != 0) {
-            int sisa = x % y;
-            System.out.println(x + " MOD " + y + " = " + sisa);
+        int a = 19;
+        int b = 2;
 
-            x = y;
-            y = sisa;
+        int old_r = a, r = b;
+        int old_s = 1, s = 0;
+        int old_t = 0, t = 1;
 
+        while (r != 0) {
+            int q = old_r / r;
+
+            int temp = r;
+            r = old_r - q * r;
+            old_r = temp;
+
+            temp = s;
+            s = old_s - q * s;
+            old_s = temp;
+
+            temp = t;
+            t = old_t - q * t;
+            old_t = temp;
         }
 
-        System.out.println("GCD = " + x);
+        System.out.println("GCD = " + old_r);
+        System.out.println("S = " + old_s);
+        System.out.println("T = " + old_t);
+
+        System.out.println(a + "(" + old_s + ") + " + b + "(" + old_t + ") = "
+                + (a * old_s + b * old_t));
     }
 }
